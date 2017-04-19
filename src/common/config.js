365df28e69ae7b57e106fa693c3a583540e2b2ff
semicoyoung <eetyang@163.com>
2017-04-19 14:58:41 +0800

    init function get chapter list and article content

--- src/common/config.js
+++ src/common/config.js
@@ -10,4 +10,411 @@ module.exports = {
   },
 
 
+
+
+  //let children = $('#wrapper').children().last();
+  children: {
+    '0': {
+      type: 'tag',
+      name: 'div',
+      attribs: { id: 'footer', name: 'footer' },
+      children: [ [Object], [Object], [Object], [Object], [Object] ],
+      next: {
+        data: '\r\n\t',
+        type: 'text',
+        next: null,
+        prev: [Circular],
+        parent: [Object]
+      },
+      prev: {
+        data: '\r\n\t\t',
+        type: 'text',
+        next: [Circular],
+        prev: [Object],
+        parent: [Object]
+      },
+      parent: {
+        type: 'tag',
+        name: 'div',
+        attribs: [Object],
+        children: [Object],
+        next: [Object],
+        prev: [Object],
+        parent: [Object]
+      }
+    },
+    options: {
+      withDomLvl1: true,
+      normalizeWhitespace: false,
+      xmlMode: false,
+      decodeEntities: true
+    },
+    _root: {
+      '0': {
+        type: 'root',
+        name: 'root',
+        attribs: {},
+        children: [Object],
+        next: null,
+        prev: null,
+        parent: null
+      },
+      options: {
+        withDomLvl1: true,
+        normalizeWhitespace: false,
+        xmlMode: false,
+        decodeEntities: true
+      },
+      length: 1,
+      _root: [Circular]
+    },
+    length: 1,
+    prevObject: {
+      '0': {
+        type: 'script',
+        name: 'script',
+        attribs: {},
+        children: [Object],
+        next: [Object],
+        prev: [Object],
+        parent: [Object]
+      },
+      '1':
+      { type: 'tag',
+        name: 'div',
+        attribs: [Object],
+        children: [Object],
+        next: [Object],
+        prev: [Object],
+        parent: [Object]
+      },
+      '2':
+      { type: 'tag',
+        name: 'div',
+        attribs: [Object],
+        children: [Object],
+        next: [Object],
+        prev: [Object],
+        parent: [Object]
+      },
+      '3':
+      { type: 'tag',
+        name: 'div',
+        attribs: [Object],
+        children: [Object],
+        next: [Object],
+        prev: [Object],
+        parent: [Object]
+      },
+      '4':
+      { type: 'tag',
+        name: 'div',
+        attribs: [Object],
+        children: [Object],
+        next: [Object],
+        prev: [Object],
+        parent: [Object]
+      },
+      '5':
+      { type: 'tag',
+        name: 'div',
+        attribs: [Object],
+        children: [Object],
+        next: [Object],
+        prev: [Object],
+        parent: [Object]
+      },
+      '6':
+      { type: 'tag',
+        name: 'div',
+        attribs: [Object],
+        children: [Object],
+        next: [Object],
+        prev: [Object],
+        parent: [Object]
+      },
+      options:
+      { withDomLvl1: true,
+        normalizeWhitespace: false,
+        xmlMode: false,
+        decodeEntities: true
+      },
+      _root: { '0': [Object], options: [Object], length: 1, _root: [Circular] },
+      length: 7,
+      prevObject:
+      { '0': [Object],
+        options: [Object],
+        _root: [Object],
+        length: 1,
+        prevObject: [Object]
+      }
+    }
+  },
+
+
+
+  //let children = $('#wrapper').children().last().prevObject['5'];
+  element: {
+    type: 'tag',
+    name: 'div',
+    attribs: { class: 'box_con' },
+    children: [
+      {
+        data: '\r\n\t\t\t',
+        type: 'text',
+        next: [Object],
+        prev: null,
+        parent: [Circular]
+      },
+      { type: 'tag',
+        name: 'div',
+        attribs: [Object],
+        children: [Object],
+        next: [Object],
+        prev: [Object],
+        parent: [Circular]
+      },
+      { data: '\r\n\t\t',
+        type: 'text',
+        next: null,
+        prev: [Object],
+        parent: [Circular]
+      }
+      ],
+    next: {
+      data: '\r\n\t\t',
+      type: 'text',
+      next: {
+        type: 'tag',
+        name: 'div',
+        attribs: [Object],
+        children: [Object],
+        next: [Object],
+        prev: [Circular],
+        parent: [Object]
+      },
+      prev: [Circular],
+      parent: {
+        type: 'tag',
+        name: 'div',
+        attribs: [Object],
+        children: [Object],
+        next: [Object],
+        prev: [Object],
+        parent: [Object]
+      }
+    },
+  prev: {
+    data: '\r\n\t\t\r\n\t\t',
+    type: 'text',
+    next: [Circular],
+    prev:
+    { type: 'tag',
+      name: 'div',
+      attribs: [Object],
+      children: [Object],
+      next: [Circular],
+      prev: [Object],
+      parent: [Object] },
+    parent:
+    { type: 'tag',
+      name: 'div',
+      attribs: [Object],
+      children: [Object],
+      next: [Object],
+      prev: [Object],
+      parent: [Object] }
+  },
+  parent:
+  { type: 'tag',
+    name: 'div',
+    attribs: { id: 'wrapper' },
+    children:
+      [ [Object],
+        [Object],
+        [Object],
+        [Object],
+        [Object],
+        [Object],
+        [Object],
+        [Object],
+        [Object],
+        [Object],
+        [Object],
+        [Circular],
+        [Object],
+        [Object],
+        [Object] ],
+    next:
+    { data: '\r\n',
+      type: 'text',
+      next: null,
+      prev: [Circular],
+      parent: [Object] },
+    prev:
+    { data: '\r\n',
+      type: 'text',
+      next: [Circular],
+      prev: null,
+      parent: [Object] },
+    parent:
+    { type: 'tag',
+      name: 'body',
+      attribs: {},
+      children: [Object],
+      next: [Object],
+      prev: [Object],
+      parent: [Object] } }
+  },
+
+
+  //let children = $('#wrapper').children().last().prevObject['5'].children[1];
+
+  inner: [ { data: '\r\n\t\t\t',
+    type: 'text',
+    next:
+    { type: 'tag',
+      name: 'div',
+      attribs: [Object],
+      children: [Object],
+      next: [Object],
+      prev: [Circular],
+      parent: [Object] },
+    prev: null,
+    parent:
+    { type: 'tag',
+      name: 'div',
+      attribs: [Object],
+      children: [Circular],
+      next: [Object],
+      prev: [Object],
+      parent: [Object] } },
+    { type: 'tag',
+      name: 'div',
+      attribs: { id: 'list' },
+      children: [ [Object], [Object], [Object] ],
+      next:
+      { data: '\r\n\t\t',
+        type: 'text',
+        next: null,
+        prev: [Circular],
+        parent: [Object] },
+      prev:
+      { data: '\r\n\t\t\t',
+        type: 'text',
+        next: [Circular],
+        prev: null,
+        parent: [Object] },
+      parent:
+      { type: 'tag',
+        name: 'div',
+        attribs: [Object],
+        children: [Circular],
+        next: [Object],
+        prev: [Object],
+        parent: [Object] } },
+    { data: '\r\n\t\t',
+      type: 'text',
+      next: null,
+      prev:
+      { type: 'tag',
+        name: 'div',
+        attribs: [Object],
+        children: [Object],
+        next: [Circular],
+        prev: [Object],
+        parent: [Object] },
+      parent:
+      { type: 'tag',
+        name: 'div',
+        attribs: [Object],
+        children: [Circular],
+        next: [Object],
+        prev: [Object],
+        parent: [Object] } }
+  ],
+
+  //let children = children.prevObject['5'].children[1].children[1]
+  children3: { type: 'tag',
+    name: 'div',
+    attribs: { id: 'list' },
+    children:
+      [ { data: '\r\n\t\t\t\t',
+        type: 'text',
+        next: [Object],
+        prev: null,
+        parent: [Circular] },
+        { type: 'tag',
+          name: 'dl',
+          attribs: {},
+          children: [Object],
+          next: [Object],
+          prev: [Object],
+          parent: [Circular] },
+        { data: '\r\n\t\t\t',
+          type: 'text',
+          next: null,
+          prev: [Object],
+          parent: [Circular] } ],
+    next:
+    { data: '\r\n\t\t',
+      type: 'text',
+      next: null,
+      prev: [Circular],
+      parent:
+      { type: 'tag',
+        name: 'div',
+        attribs: [Object],
+        children: [Object],
+        next: [Object],
+        prev: [Object],
+        parent: [Object] } },
+    prev:
+    { data: '\r\n\t\t\t',
+      type: 'text',
+      next: [Circular],
+      prev: null,
+      parent:
+      { type: 'tag',
+        name: 'div',
+        attribs: [Object],
+        children: [Object],
+        next: [Object],
+        prev: [Object],
+        parent: [Object] } },
+    parent:
+    { type: 'tag',
+      name: 'div',
+      attribs: { class: 'box_con' },
+      children: [ [Object], [Circular], [Object] ],
+      next:
+      { data: '\r\n\t\t',
+        type: 'text',
+        next: [Object],
+        prev: [Circular],
+        parent: [Object] },
+      prev:
+      { data: '\r\n\t\t\r\n\t\t',
+        type: 'text',
+        next: [Circular],
+        prev: [Object],
+        parent: [Object] },
+      parent:
+      { type: 'tag',
+        name: 'div',
+        attribs: [Object],
+        children: [Object],
+        next: [Object],
+        prev: [Object],
+        parent: [Object] } }
+  },
+
+  //let children = children.prevObject['5'].children[1].children[1].children[1]
 }
+
+
+
+
+
+
+
